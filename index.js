f718f9c673ce2a51aaee397cf4b2d130e71bae20
Max Ratmeyer <17440617+MaxRatmeyer@users.noreply.github.com>
2020-12-24 02:41:12 +0000

    Updated discord.js version

--- index.js
+++ index.js
@@ -18,8 +18,6 @@ client.once('ready', () => {
     // Matches join to create channel ID with the parent category to create the private channel under
     channelConfig.set('780570910171463680', '627381429877211150')
 
-    client.user.setActivity("!vanity"); 
-
     console.log('MaxBot connected and ready to go!')
 })
 
